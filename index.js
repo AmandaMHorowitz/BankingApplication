e65d72eb31d71830401de0dfb672204166f67ab1
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-17 10:54:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ function Spa() {
   const HashRouter    = ReactRouterDOM.HashRouter;
   return (
     <HashRouter>
+        <NavBar/>
         <div>
-          <h1>Routing - Hello Enrique</h1>
-          <Link to="/">Home</Link> --
-          <Link to="/CreateAccount/">CreateAccount</Link> --
-          <Link to="/login/">Login</Link> --
-          <Link to="/deposit/">Deposit</Link> --
-          <Link to="/withdraw/">Withdraw</Link> --
-          <Link to="/balance/">Balance</Link> --
-          <Link to="/alldata/">AllData</Link> --
           <hr/>
         </div>
       <UserContext.Provider value={{users:[{name:'Horowitz',email:'Horowitz@gmail.com',password:'secret',balance:100}]}}>
