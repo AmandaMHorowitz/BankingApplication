92d62ed0fa25dede3ef0b72c5444a89201b31a7a
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-19 14:43:44 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -5,13 +5,28 @@ function Deposit(){
   const [balance, setBalance]         = React.useState(0);
   const ctx                           = React.useContext(UserContext);
   
+  function validate(field, label){
+      if (!field) {
+        setStatus('Error: ' + label);
+        setTimeout(() => setStatus(''),3000);
+        return false;
+      }
+      return true;
+  }
   
   function handleSubmit(){
     let newBalance = balance + depositAmt;
     ctx.users.push({newBalance});
-  };
+    setShow(false);
+  }    
 
-  
+  function clearForm(){
+    setName('');
+    setEmail('');
+    setPassword('');
+    setShow(true);
+  }
+
   return (
     <Card
       bgcolor="dark"
@@ -23,7 +38,13 @@ function Deposit(){
               Deposit<br/>
               <input type="input" className="form-control" id="depositAmt" placeholder="Enter Deposit Amount" value={depositAmt} onChange={e => setBalance(e.currentTarget.value)} /><br/>
               <button type="submit" className="btn btn-light" onClick={handleSubmit}>Submit Deposit</button>
-              
+              </>
+            ):(
+              <>
+              <h5>`newBalance`</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Make Another Deposit</button>
+              </>
+            )}
     />
   );
 }
