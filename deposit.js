25d55ccb85e5951c10afdf10acf9e2088e157452
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-19 12:28:16 -0400

    Update deposit.js
    Update deposit.js
    
    Update deposit.js
    
    Update deposit.js
    
    Update deposit.js
    
    Update deposit.js
    
    Update deposit.js
    
    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,9 +1,43 @@
 function Deposit(){
+  const [depositAmt, setDepositAmt] = React.useState(0);
+  const [totalState, setTotalState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+  const [atmMode, setAtmMode] = React.useState('');
+  const [validTransaction, setValidTransaction] = React.useState(true);
+
+  let status = `Account Balance $ ${totalState} `;
+  console.log(`Account Rendered with isDeposit: ${isDeposit}`);
+  const handleChange = (event) => {
+    console.log(Number(event.target.value));
+    if (Number(event.target.value) <= 0) {
+      return setValidTransaction(false);
+     else {
+       return setValidTransaction(true);
+    }
+    setDepositAmt(Number(event.target.value));
+  };
+  const handleSubmit = (event) => {
+    let newTotal = isDeposit ? totalState + depositAmt : totalState - depositAmt;
+    setTotalState(newTotal);
+    setValidTransaction(false);
+    event.preventDefault();
+  };
+
+  const handleModeSelect = (event) => {
+    console.log(event.target.value);
+    setAtmMode(event.target.value);
+    setValidTransaction(false);
+    if (event.target.value === 'Deposit') {
+      setIsDeposit(true);
+    } else {
+      setIsDeposit(false);
+    }
+  };
   return (
     <Card
       bgcolor="dark"
       header="Deposit"
-      title="Balance:"
+      title="Balance: "
       text="Deposit your balance here"
     />
   );
