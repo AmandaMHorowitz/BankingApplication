3134680332b4f3c0ff19e5ba5dbe9fa059680b1d
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-19 10:15:24 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -1,5 +1,41 @@
 function Deposit(){
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState('');
+  const [balance, setBalance]   = React.useState('');
+  const [deposit, setDeposit]   = React.useState('');
+  const ctx = React.useContext(UserContext);  
+
+  function validate(field, label){
+      if (!field) {
+        setStatus('Error: ' + label);
+        setTimeout(() => setStatus(''),3000);
+        return false;
+      }
+      return true;
+  }
+
+ 
+  
+
   return (
-    <h1>Deposit</h1>
+    <Card
+      bgcolor="dark"
+      header="Deposit"
+      status={status}
+      body={show ? (  
+              <>
+              Balance<br/>
+              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+              Deposit<br/>
+              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+              <button type="submit" className="btn btn-light" onClick={handleCreate}>Deposit</button>
+              </>
+            ):(
+              <>
+              <h5>Success</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Make Another Deposit</button>
+              </>
+            )}
+    />
   )
 }
