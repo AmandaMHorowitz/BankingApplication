a402d0bf233fbb8e880d5c041f61a9ecca570333
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-20 16:33:55 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -11,6 +11,6 @@ function AllData(){
       title="Welcome to the bank"
       text="You can move around using the navigation bar."
       body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-    />   
+    />
   ); 
 }
