34877687521e2057e25b5ea99307e4a250de3931
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-20 16:23:49 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -5,26 +5,25 @@ function AllData(){
     <h5>All Data in Store</h5>
     {JSON.stringify(ctx)}<br/>
     
-   <div class="card-group">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Name</h5>
-        <p class="card-text">ctx.name</p>
-      </div>
+    <div class="card-group">
+       <div class="card">
+          <div class="card-body">
+             <h5 class="card-title">Name</h5>
+             <p class="card-text">ctx.name</p>
+          </div>
+       </div>
+       <div class="card">
+          <div class="card-body">
+             <h5 class="card-title">Email Address</h5>
+             <p class="card-text">ctx.email</p>
+          </div>
+       </div>
+       <div class="card">
+          <div class="card-body">
+             <h5 class="card-title">Password</h5>
+             <p class="card-text">ctx.password</p>
+          </div>
+       </div>
     </div>
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Email Address</h5>
-        <p class="card-text">ctx.email</p>
-      </div>
-    </div>
-    <div class="card">
-       <div class="card-body">
-        <h5 class="card-title">Password</h5>
-        <p class="card-text">ctx.password</p>
-      </div>
-    </div>
-  </div>
-
   ); 
 }
