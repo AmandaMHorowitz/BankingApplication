36c676f96d86da56092638c6ede6de325912a41e
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-20 16:31:16 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -5,25 +5,12 @@ function AllData(){
     <h5>All Data in Store</h5>
     {JSON.stringify(ctx)}<br/>
     
-    <div class="card-group">
-       <div class="card">
-          <div class="card-body">
-             <h5 class="card-title">Name</h5>
-             <p class="card-text">ctx.name</p>
-          </div>
-       </div>
-       <div class="card">
-          <div class="card-body">
-             <h5 class="card-title">Email Address</h5>
-             <p class="card-text">ctx.email</p>
-          </div>
-       </div>
-       <div class="card">
-          <div class="card-body">
-             <h5 class="card-title">Password</h5>
-             <p class="card-text">ctx.password</p>
-          </div>
-       </div>
-    </div>
+    <Card
+      txtcolor="black"
+      header="Bank of Horowitz"
+      title="Welcome to the bank"
+      text="You can move around using the navigation bar."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />   
   ); 
 }
