edfe04bc7ccf0c8f5b34447b2e542dc9a8e0abc9
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-20 16:42:37 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -1,23 +1,20 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
   return (
-    <div class="card-group">
+   <div class="card-group">
      <div class="card">
         <div class="card-body">
            <h5 class="card-title">Name</h5>
-           <p class="card-text">ctx.name</p>
         </div>
      </div>
      <div class="card">
         <div class="card-body">
            <h5 class="card-title">Email Address</h5>
-           <p class="card-text">ctx.email</p>
         </div>
      </div>
      <div class="card">
         <div class="card-body">
            <h5 class="card-title">Password</h5>
-           <p class="card-text">ctx.password</p>
         </div>
      </div>
    />
