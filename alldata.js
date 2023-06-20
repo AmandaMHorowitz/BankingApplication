381f352b7a4bf7409ce36a582ddc92f3683fb5ad
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-20 16:40:18 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -1,12 +1,25 @@
 function AllData(){
   const ctx = React.useContext(UserContext);
   return (
-    <Card
-      txtcolor="black"
-      header="Bank of Horowitz"
-      title="Welcome to the bank"
-      text="You can move around using the navigation bar."
-      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
-    />
+    <div class="card-group">
+     <div class="card">
+        <div class="card-body">
+           <h5 class="card-title">Name</h5>
+           <p class="card-text">ctx.name</p>
+        </div>
+     </div>
+     <div class="card">
+        <div class="card-body">
+           <h5 class="card-title">Email Address</h5>
+           <p class="card-text">ctx.email</p>
+        </div>
+     </div>
+     <div class="card">
+        <div class="card-body">
+           <h5 class="card-title">Password</h5>
+           <p class="card-text">ctx.password</p>
+        </div>
+     </div>
+   />
   ); 
 }
