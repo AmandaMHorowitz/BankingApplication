d127ca9b056340645a2fb57ab1be9eb7717682da
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-20 12:01:32 -0400

    updates

--- alldata.js
+++ alldata.js
@@ -4,11 +4,27 @@ function AllData(){
     <>
     <h5>All Data in Store</h5>
     {JSON.stringify(ctx)}<br/>
-    <Card
-      bgcolor="dark"
-      header="User Information"
-      body=ctx
-            )}
-    />
-  );
- }
+    
+   <div class="card-group">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Name</h5>
+        <p class="card-text">ctx.name</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Email Address</h5>
+        <p class="card-text">ctx.email</p>
+      </div>
+    </div>
+    <div class="card">
+       <div class="card-body">
+        <h5 class="card-title">Password</h5>
+        <p class="card-text">ctx.password</p>
+      </div>
+    </div>
+  </div>
+
+  ); 
+}
