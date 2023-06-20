c63328f4069167518ecac0a75947a7beae2c9dcf
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-06-20 19:04:51 -0400

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -49,7 +49,7 @@ function CreateAccount(){
               <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
               Password<br/>
               <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              <button type="submit" className="btn btn-light" onClick={handleCreate} disabled=true>Create Account</button>
               </>
             ):(
               <>
